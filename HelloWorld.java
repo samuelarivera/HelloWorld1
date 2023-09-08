23fd53b485480e088d16679be713f438aed4afd0
Samuel Rivera <samuel.rivera@mrjaffesclass.com>
2023-09-08 11:18:29 -0700

    Datatypes code, intefer, float, and boolean variables

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,9 @@ public class HelloWorld
 {
     public static void main(String[] args) 
     {
-        System.out.println("Hello World!") ;
+            int i;   
+            float f = 2.5f;
+            boolean flag = true;
     }
 }
 
